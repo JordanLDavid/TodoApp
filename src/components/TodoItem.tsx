87c9e3cf7e3a9b43ac63cba5168a91a9bb448161
Jordan Lenard David <jordan_david02@hotmail.com>
2023-10-08 17:29:26 -0400

    Fix column hiding issue

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,41 +1,41 @@
 import IconButton from '@mui/material/IconButton'
-import Stack from '@mui/material/Stack'
 import SaveIcon from '@mui/icons-material/Save';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CancelIcon from '@mui/icons-material/Cancel';
 import { useState } from 'react';
 import TextField from '@mui/material/TextField';
+import { Grid } from '@mui/material';
 
 export default function TodoItem({item, onDelete, onUpdate }) {
-const [activity, setActivity] = useState(0);
+const [isEdit, setEditable] = useState(false);
 const [updatedTodo, editTodo] = useState(item.todo);
-const [todoWidth, setTodoWidth] = useState('0');
-const [editTodoWidth, setEditTodoWidth] = useState('auto');
 
   return (
-    <Stack direction="row" spacing={3}>
+    <div>
+    <Grid container spacing={3}>
         <br/>
-        <div hidden={activity!==0} style={{ width: todoWidth }}> 
+        { !isEdit && (
+        <Grid item xs={12}> 
             <p> {item.todo} </p> 
-            <IconButton onClick={()=>{setActivity(1); setTodoWidth('0%'); setEditTodoWidth('100%')}}>
+            <IconButton onClick={()=>{setEditable(true);}}>
                 <EditIcon/>
             </IconButton>
             <IconButton onClick={()=>onDelete(item.id)}>
                 <DeleteIcon/>
             </IconButton>
-        </div>
-        <div hidden={activity!==1} style={{ width: editTodoWidth}}>
+        </Grid> ) }
+        { isEdit && (
+        <Grid item xs={12}> 
         <TextField color="primary" label="Edit Todo" variant="outlined" name="EditTodo" value={updatedTodo} onChange={event => { editTodo(event.target.value)}}></TextField>
-            <IconButton onClick={()=>{setActivity(0); onUpdate(item.id, updatedTodo); setTodoWidth('100%'); setEditTodoWidth('0%')}}>
+            <IconButton onClick={()=>{setEditable(false); onUpdate(item.id, updatedTodo);}}>
                 <SaveIcon/>
             </IconButton>
-            <IconButton onClick={()=>{setActivity(0); editTodo(item.todo); setTodoWidth('100%'); setEditTodoWidth('0%')}}>
+            <IconButton onClick={()=>{setEditable(false);}}>
                 <CancelIcon/>
             </IconButton>
-        </div>
-
-
-    </Stack>
+        </Grid>)}
+    </Grid>
+    </div>
   )
 }
